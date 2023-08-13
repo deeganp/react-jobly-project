cdde05757c66b36130f02bf489bbb60ec6ea3a6c
deeganp <deeganp.motro@gmail.com>
2023-08-13 13:54:34 -0400

    first

---
+++ frontend/build/precache-manifest.77e4f9cc53191623228617b93d459ce5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f63d67fa86e20163369d0e2051561d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "979cdeabd4a8810d49e4",
+    "url": "/static/css/2.560b9325.chunk.css"
+  },
+  {
+    "revision": "65d0864ae9ff046eaaed",
+    "url": "/static/css/main.658eb3e8.chunk.css"
+  },
+  {
+    "revision": "979cdeabd4a8810d49e4",
+    "url": "/static/js/2.19b9ca42.chunk.js"
+  },
+  {
+    "revision": "395cc4b3602716a6dc8075912d30f948",
+    "url": "/static/js/2.19b9ca42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65d0864ae9ff046eaaed",
+    "url": "/static/js/main.93e28ec2.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.93e28ec2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e157b3318261263bb32a",
+    "url": "/static/js/runtime-main.ca09d11f.js"
+  }
+]);
